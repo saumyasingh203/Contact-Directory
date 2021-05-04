fd44ebad3f7e694388e73f4a954ee104b8b384d7
Saumya Singh <saumya.singh203@gmail.com>
2021-05-04 17:31:08 +0530

    deployment3

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const config = require('production');
+const config = require('config');
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
